70cb4782f19d90d6a90a7de70c39b52702a1b5ff
Andrew Shannon <andrew@Andrews-iMac.lan>
2020-10-15 10:08:18 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* How to use jQuery 
+$(selector).action();
+*/
+//$(document).ready(() => {
+// following shorthand for document ready method
+$(() => {
+    $('h1').slideUp(1000).slideDown(1000);
+
+    $('#main').css({
+        color: "red",
+        fontSize: 23
+    });
+    
+    $('.content').css({
+        color: "pink",
+        fontSize: 50
+    });
+    
+    $('h1').click(function() {
+        $('.content').css({
+            color: "blue",
+            fontSize: 20
+        });
+    });
+});
+
+$(window).on("load", () => {
+    console.log("FULLY LOADED INCLUDING GRAPHICS")
+});
