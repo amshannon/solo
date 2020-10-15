907af815df221c82ddc64c5287bb970aa5560fed
Andrew Shannon <andrew@Andrews-iMac.lan>
2020-10-15 14:49:58 +0100

    added home section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*================================================
     Preloader
 ================================================*/
-$(window).on('load', () => {
+$(window).on('load', () => { // make sure whole site is preloaded
     $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
 });
