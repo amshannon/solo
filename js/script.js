3268f63228e35d321a5e3e6304aadd10a682b496
amshannon <andrewmartinshannon@gmail.com>
2020-10-15 18:34:12 +0100

    added first part of team section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', () => { // make sure whole site is preloaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+/*================================================
+    Team
+================================================*/
+$(() => { // short form of $(document).ready method
+    $('#team-members').owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 500,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        dots: false
+    });
+})
