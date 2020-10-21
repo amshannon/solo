2869321aef67200718b71d8bcbeca7175892af7e
amshannon <andrewmartinshannon@gmail.com>
2020-10-21 11:10:48 +0100

    added team section with bootstrap progress bars and jquery animation, using waypoints plugin to initiate animation.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
 /*================================================
     Preloader
 ================================================*/
-$(window).on('load', () => { // make sure whole site is preloaded
-    $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow');
+$(window).on("load", () => {
+  // make sure whole site is preloaded
+  $("#status").fadeOut();
+  $("#preloader").delay(350).fadeOut("slow");
 });
 
 /*================================================
     Team
 ================================================*/
-$(() => { // short form of $(document).ready method
-    $('#team-members').owlCarousel({
-        items: 2,
-        autoplay: true,
-        smartSpeed: 500,
-        loop: true,
-        autoplayHoverPause: true,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        dots: false
-    });
-})
+$(function () {
+  // short form of $(document).ready method
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 500,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    dots: false,
+  });
+});
+
+/*================================================
+    Progresss Bars
+================================================*/
+$(() => {
+  // alternative short form of $(document).ready method
+
+  $("#progress-bars").waypoint(
+    function () {
+      $(".progress-bar").each(function () {
+        $(this).animate(
+          {
+            width: $(this).attr("aria-valuenow") + "%",
+          },
+          1000
+        );
+      });
+      this.destroy();
+    },
+    {
+      offset: "bottom-in-view",
+    }
+  );
+});
