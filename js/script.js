ffe6e0c138af039ce1a1d4d72391132e87ae350f
amshannon <andrewmartinshannon@gmail.com>
2020-10-26 12:40:09 +0000

    added testimonial section with owl carousel  slider

--- js/script.js
+++ js/script.js
@@ -64,26 +64,45 @@ $(function () {
     Portfolio - Isotope Filter
 ================================================*/
 $(window).on("load", function () {
-    // init Isotope
-    var $grid = $('#isotope-container').isotope({
-        // options
-    });
-    // filter items on button click
-    $('#isotope-filters').on( 'click', 'button', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-        // active button
-        $("#isotope-filters").find('.active').removeClass('active');
-        $(this).addClass('active');
-    });
+  // init Isotope
+  var $grid = $("#isotope-container").isotope({
+    // options
+  });
+  // filter items on button click
+  $("#isotope-filters").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+    // active button
+    $("#isotope-filters").find(".active").removeClass("active");
+    $(this).addClass("active");
+  });
 });
 /* Magnifier */
 $(function () {
-    $("#portfolio-wrapper").magnificPopup({
-        delegate: 'a', // child items selector, but clicking on it popup will open
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+  $("#portfolio-wrapper").magnificPopup({
+    delegate: "a", // child items selector, but clicking on it popup will open
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
+/*================================================
+    Testimonials
+================================================*/
+$(function () {
+  // short form of $(document).ready method
+  $("#testimonial-slider").owlCarousel({
+    items: 1,
+    autoplay: false,
+    smartSpeed: 500,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    dots: false,
+  });
 });
